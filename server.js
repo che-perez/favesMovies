c3f6b5428194618a5efd30201bba0d314c83892e
Josue Perez <che.perez3@gmail.com>
2018-03-14 11:57:31 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(PORT, () => {
+    console.log(`Knock Knock on ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send("Hola Mundo");
+})
+
+app.use('*', (req,res) => {
+    res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        error: err,
+        message: err.message,
+    });
+});
