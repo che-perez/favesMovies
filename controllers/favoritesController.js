01e1f7514490ede6235175f21096c1791c14ae64
Josue Perez <che.perez3@gmail.com>
2018-03-14 17:36:06 -0400

    add favoritesController

---
+++ controllers/favoritesController.js
@@ -0,0 +1,61 @@
+const Favorites = require("../models/Favorites");
+
+const favoritesController = {};
+
+favoritesController.index = (req, res) => {
+  Favorites.findAll()
+    .then(movies => {
+      res.json({
+        message: "OK",
+        data: { movies }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ message: "400", err });
+    });
+};
+
+favoritesController.create = (req, res) => {
+  Favorites.create({
+    original_title: req.body.original_title,
+    overview: req.body.overview,
+    poster_path: req.body.poster_path,
+    backdrop_path: req.body.backdrop_path,
+    movie_id: req.body.movie_id
+  })
+    .then(movie => {
+      res.json({
+        message: "OK",
+        data: { movie }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ message: "400", err });
+    });
+};
+
+favoritesController.show = (req, res) => {
+  Favorites.findById(req.params.id)
+    .then(movie => {
+        res.json({
+            message: 'OK',
+            data: { movie },
+        });
+    }).catch(err => {
+        res.status(400).json({ message: '400', err });
+    });
+};
+
+favoritesController.destroy = (req, res) => {
+    Favorites.destroy(req.params.id)
+        .then(() => {
+            res.json({ message: 'movie removed from favorites' });
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+};
+
+module.exports = favoritesController;
