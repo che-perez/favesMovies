b7c7cddd2f1b18a3972de0dd5609fd2fd1630574
Josue Perez <che.perez3@gmail.com>
2018-03-16 18:44:04 -0400

    setup React client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Front End Faves Movies
+      </div>
+    );
+  }
+}
+
+export default App;
