176fbf1cf72bed475e3b835c9305643554860600
Josue Perez <che.perez3@gmail.com>
2018-03-19 18:10:41 -0400

    styling MoviesDetails component

--- client/src/components/MoviesDetails.jsx
+++ client/src/components/MoviesDetails.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Styled from 'styled-components';
+
+
+const Header = Styled.div`
+background: #777777;
+color: white;
+text-align: center;
+padding: 10px 0px;
+letter-spacing: 2px;
+font-weight: bold;
+`;
+
+const Container = Styled.div`
+  width: 70vw;
+
+`;
+
 
 const MovieDetails = function() {
-  return <p>Movie Details...</p>;
+  return (
+    <Container>
+      <Header> DETAILS</Header>
+   <p>Movie Details...</p>
+   </Container>
+
+  )
 };
 
 export default MovieDetails;
