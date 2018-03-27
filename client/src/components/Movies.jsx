8c841ec7a0509c64c25c8679cdb4b4d5a2a908aa
Josue Perez <che.perez3@gmail.com>
2018-03-27 16:54:58 -0400

    change movies controller to a class and add the post method

--- client/src/components/Movies.jsx
+++ client/src/components/Movies.jsx
@@ -1,22 +1,55 @@
-import React from "react";
-import Styled from 'styled-components';
-import Moment from 'react-moment';
+import React, { Component } from "react";
+import Styled from "styled-components";
+import Moment from "react-moment";
 
-const Movies = function(props) {
-  let poster;
-  if (props.poster) {
-    poster = (
-      <img
-        src={`https://image.tmdb.org/t/p/w200${props.poster}`}
-        alt={props.title}
-        width="150"
-      />
-    );
-  } else {
-    poster = <img src="http://via.placeholder.com/150x225" alt={props.title} />;
+class Movies extends Component {
+  constructor() {
+    super();
+
+    this.addToFavorites = this.addToFavorites.bind(this);
   }
 
-  const Box = Styled.div`
+
+
+addToFavorites(id) {
+  let faveMovie = this.props.list.filter(movie => movie.id === id);
+
+    let data = {
+      title: faveMovie[0].title,
+      overview: faveMovie[0].overview,
+      poster_path: faveMovie[0].poster_path,
+      backdrop_path: faveMovie[0].backdrop_path,
+      movie_id: faveMovie[0].id,
+      release_date: faveMovie[0].release_date
+    };
+    console.log('fave movie', data);
+  fetch("/api/favorites", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then(res => res.json())
+}
+
+  render() {
+    let poster;
+    if (this.props.poster) {
+      poster = (
+        <img
+          src={`https://image.tmdb.org/t/p/w200${this.props.poster}`}
+          alt={this.props.title}
+          width="150"
+        />
+      );
+    } else {
+      poster = (
+        <img src="http://via.placeholder.com/150x225" alt={this.props.title} />
+      );
+    }
+
+
+    const Box = Styled.div`
     display: flex;
     align-items: flex-start;
     background: white;
@@ -26,7 +59,7 @@ const Movies = function(props) {
     position: relative;
   `;
 
-  const Fav = Styled.div`
+    const Fav = Styled.div`
     font-size: 20px;
     align-self: flex-end;
     margin: 0px 10px 0px 0px;
@@ -39,25 +72,30 @@ const Movies = function(props) {
 
   `;
 
-  const Title = Styled.p`
+    const Title = Styled.p`
     margin-left: 10px;
     font-size 1.5em;
   `;
-  const Year = Styled.p`
+    const Year = Styled.p`
     margin-left: 10px;
     font-weight: 100;
   `;
 
-  return (
-    <Box onClick={() => props.selectMovie(props.id,props.list)} >
-      {poster}
-      <div>
-      <Title>{props.title}</Title>
-      <Year><Moment format="YYYY">{props.release_date}</Moment></Year>
-      </div>
-      <Fav><i className="far fa-heart"></i></Fav>
-    </Box>
-  );
-};
+    return (
+      <Box onClick={() => this.props.selectMovie(this.props.id, this.props.list)}>
+        {poster}
+        <div>
+          <Title>{this.props.title}</Title>
+          <Year>
+            <Moment format="YYYY">{this.props.release_date}</Moment>
+          </Year>
+        </div>
+        <Fav onClick={() => this.addToFavorites(this.props.id)}>
+          <i className="far fa-heart" />
+        </Fav>
+      </Box>
+    );
+  }
+}
 
 export default Movies;
